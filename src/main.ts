e3eb0eafc3420be0ccf9caa5db2a4cba00858987
Santhosh Kumar S <santhosh.kumar@ztech.io>
2020-06-02 20:44:08 +0530

    configured fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Create a Fastify App.
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // Port to run application.
+  const port = 14087;
+
+  // Listen to the port
+  await app.listen(port);
 }
 bootstrap();
